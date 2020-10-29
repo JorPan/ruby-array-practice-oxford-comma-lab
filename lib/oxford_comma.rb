d0382d65960de9e7090eccb1aad0e258ed18f7b8
JorPan <jbpanasewicz@gmail.com>
2020-10-29 17:16:06 -0600

    commit tree 205d93afa46b13a9764a2eccb26ff5713941b342

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    new_array = []
+    if array.length == 1
+        return array.join
+    elsif array.length == 2
+        return "#{array[0]} and #{array[1]}"
+    elsif array.length == 3
+        return "#{array[0]}, #{array[1]}, and #{array[2]}"
+    else
+        array.each do |word|
+            # binding.pry
+            new_array << "#{word}, "
+        end
+    end
+    new_array[-2] << "and "
+    answer = new_array.join
+    answer.pop
 end
